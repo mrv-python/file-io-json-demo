f8eb6a96340b2f9e09323424924780f65bae9bfd
mrveldkamp <colin.veldkamp@epsb.ca>
2022-09-21 13:04:33 -0600

    update

--- main.py
+++ main.py
@@ -9,14 +9,19 @@ import json
 # JSON supports all basic data, lists and dictionaries (not objects)
 
 # The dumps() method will convert data to a json string
-data = [{"x": 1, "y": 1}, {"x": 2, "y": 2}, {"x": 3, "y": 3}]
+data = [
+    {"x": 11, "y": 1}, 
+    {"x": 21, "y": 2}, 
+    {"x": 31, "y": 3}]
 json_string = json.dumps(data)
+print("DATA AS A JSON STRING")
 print(json_string)
 
-# The data as a JSON string may be easily saved to a file
+# # The data as a JSON string may be easily saved to a file
 file = open("data.txt", "w")
 file.write(json_string)
 file.close()
+
 
 # Load a JSON string from a file
 file = open("data2.txt", "r")
@@ -25,5 +30,7 @@ file.close()
 
 # the loads() method will convert a json string to data
 data2 = json.loads(json_string_from_file)
+print("DATA CONVERTED FROM JSON STRING FROM FILE")
+print(data2)
 for data in data2:
     print(data)
